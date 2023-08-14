9dccff70f88b21851c8273312c7c876dc118cb1b
siddhantparekh7 <siddhantparekh7@gmail.com>
2023-08-14 11:34:10 +0530

    Exp 2 added
    Task 1 in exp 2 i.e. struct and enum code has been added

---
+++ Exp2/Exp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exp2.StructEnum.Run();
+            //Exp2.AreaofTank.Run();
+        }
+    }
+}
---
+++ Exp2/Exp2/StructEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exp2
+{
+    class StructEnum
+    {
+        public enum RectangleType
+        {
+            Regular,
+            Square
+        }
+
+        public struct Rectangle
+        {
+            public int Width, Height;
+            public RectangleType Type; // Adding the RectangleType field
+        }
+
+        public static void Run()
+        {
+            Rectangle regularRect = new Rectangle();
+            regularRect.Width = 5;
+            regularRect.Height = 10;
+            regularRect.Type = RectangleType.Regular;
+
+            Rectangle squareRect = new Rectangle();
+            squareRect.Width = 7;
+            squareRect.Height = 7;
+            squareRect.Type = RectangleType.Square;
+
+            Console.WriteLine("Area of Regular Rectangle: " + (regularRect.Width * regularRect.Height));
+            Console.WriteLine("Area of Square Rectangle: " + (squareRect.Width * squareRect.Height));
+            Console.ReadLine();
+        }
+    }
+}
